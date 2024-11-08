d309bc454cfd3e1458eb72fb954ab8109947636d
CodyBezard <cody.bezard@gmail.com>
2024-11-08 16:32:00 +0100

    BDD créé vide

---
+++ src/main/java/bo/Acteur.java
@@ -0,0 +1,116 @@
+package bo;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
+@Table(name="acteur")
+public class Acteur implements Serializable {
+    @Id
+    private String id;
+    @Column(name="IDENTITE")
+    private String identite;
+    @Column(name="URL")
+    private String url;
+    @Column(name="HEIGHT")
+    private double height;
+    @ManyToOne
+    @JoinColumn(name="NAISSANCE_ID")
+    private Naissance naissance;
+    @ManyToMany(mappedBy = "acteurs")
+    private Set<Film> films;
+    @OneToMany(mappedBy = "acteur")
+    private Set<Role> roles;
+
+
+    public Acteur (){}
+
+    public Acteur(String id, String identite, String url, double height, Naissance naissance) {
+        this.id = id;
+        this.identite = identite;
+        this.url = url;
+        this.height = height;
+        this.naissance = naissance;
+    }
+
+    /**
+     * Getter for identite
+     * return identite
+     */
+    public String getIdentite() {
+        return identite;
+    }
+
+    /**
+     * Setter for identite
+     *
+     * @param identite to set
+     */
+    public void setIdentite(String identite) {
+        this.identite = identite;
+    }
+
+    /**
+     * Getter for url
+     * return url
+     */
+    public String getUrl() {
+        return url;
+    }
+
+    /**
+     * Setter for url
+     *
+     * @param url to set
+     */
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    /**
+     * Getter for height
+     * return height
+     */
+    public double getHeight() {
+        return height;
+    }
+
+    /**
+     * Setter for height
+     *
+     * @param height to set
+     */
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Acteur acteur = (Acteur) o;
+        return Double.compare(height, acteur.height) == 0 && Objects.equals(id, acteur.id) && Objects.equals(identite, acteur.identite) && Objects.equals(url, acteur.url) && Objects.equals(naissance, acteur.naissance) && Objects.equals(films, acteur.films) && Objects.equals(roles, acteur.roles);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, identite, url, height, naissance, films, roles);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("Acteur{");
+        sb.append("id='").append(id).append('\'');
+        sb.append(", identite='").append(identite).append('\'');
+        sb.append(", url='").append(url).append('\'');
+        sb.append(", height=").append(height);
+        sb.append(", naissance=").append(naissance);
+        sb.append(", films=").append(films);
+        sb.append(", roles=").append(roles);
+        sb.append('}');
+        return sb.toString();
+    }
+}
--- src/main/java/main/App.java
+++ src/main/java/main/App.java
@@ -1,7 +1,21 @@
 package main;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+//        System.out.println("Hello World");
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("movie_jpa");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction transaction = em.getTransaction();
+        transaction.begin();
+
+
+        transaction.commit();
+        em.close();
+        emf.close();
     }
 }
